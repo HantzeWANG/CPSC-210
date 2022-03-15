f08c81003fb9a67e2953d874c3894f886629e56e
Hanze Wang <hanzew25@student.ubc.ca>
2022-03-14 19:10:03 -0700

    phase 3. attempt 2: basic GUI

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -5,8 +5,6 @@ import persistence.JsonReader;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 
@@ -14,12 +12,11 @@ public class GUI extends JFrame {
     User user;
     private static final String JSON_STORE = "./data/user.json";
 
-    JPanel panelCont;
+    JPanel panelCont; // list of panels for cards
     JPanel beginningPanel;
-    JPanel panel2 = new JPanel();
+    JPanel logInPanel;
 
 
-    JButton button2 = new JButton("back");
     CardLayout cards = new CardLayout();
 
     public GUI() {
@@ -27,18 +24,15 @@ public class GUI extends JFrame {
         initializeFields();
         initializeFrame();
         initializeBeginningPanel();
+        initializeLogInPanel();
 
-
-
-        panel2.add(button2);
-
+        // Cards Layout
         panelCont.add(beginningPanel,"1");
-        panelCont.add(panel2,"2");
+        panelCont.add(logInPanel,"2");
+
         cards.show(panelCont,"1");
 
 
-
-        button2.addActionListener(e -> cards.show(panelCont,"1"));
     }
 
     private void initializeFields() {
@@ -70,17 +64,17 @@ public class GUI extends JFrame {
         beginningPanel = new JPanel();
         beginningPanel.setLayout(new FlowLayout(FlowLayout.CENTER,1000,70));
 
-        JLabel lab1 = new JLabel("Manage all your inventory with ease!", JLabel.LEFT);
-        lab1.setForeground(Color.RED);
-        lab1.setFont(new Font("MV Boli",Font.BOLD,41));
-        beginningPanel.add(lab1);
+        JLabel label = new JLabel("Manage all your inventory with ease!", JLabel.LEFT);
+        label.setForeground(Color.RED);
+        label.setFont(new Font("MV Boli",Font.BOLD,41));
+        beginningPanel.add(label);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = screenSize.width / 6;
 
         // Button to load from file
         JButton buttonToLoad = new JButton();
-        buttonToLoad.setText("Log in as " + user.getName());
+        buttonToLoad.setText("         Log in as " + user.getName() + "         ");
         buttonToLoad.setBounds(x,700,300,50);
         buttonToLoad.setFont(new Font("Comic Sans", Font.BOLD,25));
         buttonToLoad.setBackground(Color.WHITE);
@@ -88,7 +82,7 @@ public class GUI extends JFrame {
 
         // Button to create a new user
         JButton buttonToCreate = new JButton();
-        buttonToCreate.setText("create a new user account");
+        buttonToCreate.setText("Create a new user account");
         buttonToCreate.setBounds(x,200,300,50);
         buttonToCreate.setFont(new Font("Comic Sans", Font.BOLD,25));
         buttonToCreate.setBackground(Color.WHITE);
@@ -96,6 +90,52 @@ public class GUI extends JFrame {
 
         beginningPanel.add(buttonToLoad);
         beginningPanel.add(buttonToCreate);
+
+    }
+
+    private void initializeLogInPanel() {
+        logInPanel = new JPanel();
+        logInPanel.setLayout(null);
+//        JPasswordField password = new JPasswordField(100);
+//        password.setBounds(80,100,400,50);
+//        JTextField text = new JTextField();
+//
+//        JLabel label = new JLabel("Password for " + user.getName());
+//        label.setForeground(Color.RED);
+//        label.setFont(new Font("MV Boli",Font.BOLD,20));
+//        label.setLabelFor(password);
+//
+//        JButton logInButton = new JButton("Login");
+//
+//        JButton backButton = new JButton("Back");
+//        backButton.addActionListener(e -> cards.show(panelCont,"1"));
+//
+//        logInPanel.add(label);
+//        logInPanel.add(password);
+//        logInPanel.add(logInButton);
+//        logInPanel.add(backButton);
+
+        final JPasswordField value = new JPasswordField();
+        value.setBounds(100,75,100,30);
+        JLabel l1 = new JLabel("Username:");
+        l1.setBounds(20,20, 80,30);
+        JLabel l2 = new JLabel("Password:");
+        l2.setBounds(20,75, 80,30);
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(100,120, 80,30);
+        final JLabel name = new JLabel(user.getName());
+        name.setBounds(100,20, 100,30);
+
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(100,170, 80,30);
+        backButton.addActionListener(e -> cards.show(panelCont,"1"));
+
+        logInPanel.add(value);
+        logInPanel.add(l1);
+        logInPanel.add(l2);
+        logInPanel.add(name);
+        logInPanel.add(loginButton);
+        logInPanel.add(backButton);
 
     }
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,13 +4,12 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        //new GUI();
-        try {
-            new WarehouseApp();
-        } catch (FileNotFoundException e) {
-            //
-        }
-
+        new GUI();
+//        try {
+//            new WarehouseApp();
+//        } catch (FileNotFoundException e) {
+//            //
+//        }
 
     }
 }
